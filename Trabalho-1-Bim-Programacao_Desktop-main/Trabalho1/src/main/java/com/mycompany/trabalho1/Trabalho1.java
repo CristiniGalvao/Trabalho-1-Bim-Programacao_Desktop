ded3493667c056d682636dbf4ede9695b5879d5b
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-04-03 21:32:36 -0300

    ADD

---
+++ Trabalho-1-Bim-Programacao_Desktop-main/Trabalho1/src/main/java/com/mycompany/trabalho1/Trabalho1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho1;
+
+/**
+ *
+ * @author Gabriella 
+ */
+public class Trabalho1 {
+
+    public static void main(String[] args) {
+      new FramePrincipal().setVisible(true);
+      
+    }
+}
+  
